851767df9ca8e8696273409c6911bbe8115ddc0c
YounesEsbaymi <Headlineryounes2023@gmail.com>
2023-08-31 07:48:24 -0700

    0x14

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,25 +1,20 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+/*
+ * File: main.h
+ *
+ * Desc: Header file with  prototypes for all functions
+ *       written in  0x13-bit_manipulation directory.
+ */
 
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#endif
+#endif /* MAIN_H 
